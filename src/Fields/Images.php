ac30b071daf63eaf0dd6ec618c2d5e67c82a6ac6
Shaul <shaulhaf@users.noreply.github.com>
2020-05-12 17:42:56 -0400

    Update Images.php

--- src/Fields/Images.php
+++ src/Fields/Images.php
@@ -2,19 +2,9 @@
 
 namespace Capitalc\AdvancedNovaMediaLibrary\Fields;
 
-class Images extends Media
+class Images extends \Ebess\AdvancedNovaMediaLibrary\Fields\Media
 {
     public $component = 'parentimage';
-
-    protected $defaultValidatorRules = ['image'];
-
-    public function __construct($name, $attribute = null, callable $resolveCallback = null)
-    {
-        parent::__construct($name, $attribute, $resolveCallback);
-
-        $this->croppable();
-    }
-
 
     public function resolve($resource, $attribute = null)
     {
@@ -23,27 +13,4 @@ class Images extends Media
         }
         return parent::resolve($resource, $attribute = null);
     }
-
-    /**
-     * Do we deprecate this for SingleMediaRules?
-     * @param $singleImageRules
-     * @return Images
-     */
-    public function singleImageRules($singleImageRules): self
-    {
-        $this->singleMediaRules = $singleImageRules;
-
-        return $this;
-    }
-
-    public function croppable(bool $croppable = true): self
-    {
-        return $this->withMeta(compact('croppable'));
-    }
-
-    public function croppingConfigs(array $configs): self
-    {
-        return $this->withMeta(['croppingConfigs' => $configs]);
-    }
 }
-
